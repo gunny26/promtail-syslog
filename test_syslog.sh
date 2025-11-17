fde416c2c75401d56f5df780bcc3f85be21bb55e
Arthur Messner <arthur.messner@gmail.com>
2025-11-17 22:34:49 +0100

    rsyslog target changed to localhost

--- test_syslog.sh
+++ test_syslog.sh
@@ -2,11 +2,11 @@
 
 # change IP to real IP of rsyslog server
 # rsyslog server
-logger -p 0 -T -P 514 -n 10.0.0.1 -t 'Ubuntu' 'TCP Test message'
-logger -p 0 -d -P 514 -n 10.0.0.1 -t 'Ubuntu' 'UDP Test message'
+logger -p 0 -T -P 514 -n 10.0.0.1 -t 'Ubuntu' 'TCP Test message on port 514'
+logger -p 0 -d -P 514 -n 10.0.0.1 -t 'Ubuntu' 'UDP Test message on port 512'
 # promtail in rsyslog container
-logger -p 0 -T -P 1514 -n 10.0.0.1 -t 'Ubuntu' 'TCP Test message'
-logger -p 0 -d -P 1514 -n 10.0.0.1 -t 'Ubuntu' 'UDP Test message'
+logger -p 0 -T -P 1514 -n 10.0.0.1 -t 'Ubuntu' 'TCP Test message on port 1514'
+logger -p 0 -d -P 1514 -n 10.0.0.1 -t 'Ubuntu' 'UDP Test message on port 1514'
 # native promtail server
-logger -p 0 -T -P 5140 -n 10.0.0.1 -t 'Ubuntu' 'TCP Test message'
-logger -p 0 -d -P 5140 -n 10.0.0.1 -t 'Ubuntu' 'UDP Test message'
+logger -p 0 -T -P 5140 -n 10.0.0.1 -t 'Ubuntu' 'TCP Test message on port 5140'
+logger -p 0 -d -P 5140 -n 10.0.0.1 -t 'Ubuntu' 'UDP Test message on port 5140'
